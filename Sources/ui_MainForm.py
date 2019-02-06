b88a292a5564cba67ba07a414733c812049d84b0
clouds-catcher <bit4crush@gmail.com>
2019-02-06 16:39:23 +0300

    Added GUI features for choosing multiplication type

--- Sources/ui_MainForm.py
+++ Sources/ui_MainForm.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'UIFiles/MainForm.ui',
 # licensing of 'UIFiles/MainForm.ui' applies.
 #
-# Created: Tue Feb  5 18:48:47 2019
+# Created: Wed Feb  6 16:33:37 2019
 #      by: pyside2-uic  running on PySide2 5.12.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,18 +13,18 @@ from PySide2 import QtCore, QtGui, QtWidgets
 class Ui_MainForm(object):
     def setupUi(self, MainForm):
         MainForm.setObjectName("MainForm")
-        MainForm.resize(400, 175)
+        MainForm.resize(400, 220)
         self.input1_line = QtWidgets.QLineEdit(MainForm)
-        self.input1_line.setGeometry(QtCore.QRect(8, 20, 381, 27))
+        self.input1_line.setGeometry(QtCore.QRect(8, 78, 381, 27))
         self.input1_line.setObjectName("input1_line")
         self.input2_line = QtWidgets.QLineEdit(MainForm)
-        self.input2_line.setGeometry(QtCore.QRect(8, 68, 381, 27))
+        self.input2_line.setGeometry(QtCore.QRect(8, 126, 381, 27))
         self.input2_line.setObjectName("input2_line")
         self.result_line = QtWidgets.QLineEdit(MainForm)
-        self.result_line.setGeometry(QtCore.QRect(8, 130, 381, 27))
+        self.result_line.setGeometry(QtCore.QRect(8, 188, 381, 27))
         self.result_line.setObjectName("result_line")
         self.label = QtWidgets.QLabel(MainForm)
-        self.label.setGeometry(QtCore.QRect(171, 48, 54, 17))
+        self.label.setGeometry(QtCore.QRect(171, 106, 54, 17))
         font = QtGui.QFont()
         font.setPointSize(12)
         font.setWeight(75)
@@ -33,13 +33,26 @@ class Ui_MainForm(object):
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.solve_button = QtWidgets.QPushButton(MainForm)
-        self.solve_button.setGeometry(QtCore.QRect(155, 99, 85, 27))
+        self.solve_button.setGeometry(QtCore.QRect(8, 157, 381, 27))
         font = QtGui.QFont()
         font.setPointSize(12)
         font.setWeight(75)
         font.setBold(True)
         self.solve_button.setFont(font)
         self.solve_button.setObjectName("solve_button")
+        self.groupBox = QtWidgets.QGroupBox(MainForm)
+        self.groupBox.setGeometry(QtCore.QRect(10, 2, 381, 71))
+        self.groupBox.setFlat(True)
+        self.groupBox.setCheckable(False)
+        self.groupBox.setObjectName("groupBox")
+        self.radio_elementwise = QtWidgets.QRadioButton(self.groupBox)
+        self.radio_elementwise.setGeometry(QtCore.QRect(10, 23, 131, 22))
+        self.radio_elementwise.setChecked(True)
+        self.radio_elementwise.setObjectName("radio_elementwise")
+        self.radio_vector = QtWidgets.QRadioButton(self.groupBox)
+        self.radio_vector.setGeometry(QtCore.QRect(10, 43, 131, 22))
+        self.radio_vector.setChecked(False)
+        self.radio_vector.setObjectName("radio_vector")
 
         self.retranslateUi(MainForm)
         QtCore.QMetaObject.connectSlotsByName(MainForm)
@@ -48,4 +61,7 @@ class Ui_MainForm(object):
         MainForm.setWindowTitle(QtWidgets.QApplication.translate("MainForm", "Vectors multiplication", None, -1))
         self.label.setText(QtWidgets.QApplication.translate("MainForm", "x", None, -1))
         self.solve_button.setText(QtWidgets.QApplication.translate("MainForm", "=", None, -1))
+        self.groupBox.setTitle(QtWidgets.QApplication.translate("MainForm", "Тип умножения", None, -1))
+        self.radio_elementwise.setText(QtWidgets.QApplication.translate("MainForm", "Поэлементное", None, -1))
+        self.radio_vector.setText(QtWidgets.QApplication.translate("MainForm", "Векторное", None, -1))
 
